f4d0d2b8105a31a7bb93098d3517f38355d7d9a0
alex <obal25ms@student.ju.se>
2026-02-19 11:38:46 +0100

    Add: test javascript and product page

---
+++ js/cart.js
@@ -0,0 +1,8 @@
+// Element paths
+const cartTotalPriceElement = document.getElementById("cart-total-price");
+
+//---CODE----
+
+//Calculate price
+let totalPrice = 700;
+cartTotalPriceElement.innerText = "TOTAL: " + totalPrice + "kr";
